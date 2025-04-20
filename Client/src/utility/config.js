c74ccc4efae6cd5133d3ec8de3c92610b6d408c6
Sameer Srivastava <anilanita07@gmail.com>
2025-04-20 15:04:35 +0530

    AI screening feature of resumes

--- Client/src/utility/config.js
+++ Client/src/utility/config.js
@@ -1,2 +1,3 @@
 export const BASEURL = "https://aurjobsjobsboardbackend.onrender.com"
+export const BASEURL1 = "https://aurjobssourcingbackend.onrender.com"
     // export const BASEURL = "http://ec2-13-201-226-173.ap-south-1.compute.amazonaws.com:3000"
